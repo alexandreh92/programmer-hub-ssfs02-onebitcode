9477f4e3232a9e701894c4fda956fe326f8d7272
Alexandre <eng.alexandreh@gmail.com>
2020-01-28 00:44:03 -0300

    Second lesson

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def liked?(user, post)
+    user.likes.find_by(post_id: post.id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :likes
-
+  has_many :likes, dependent: :destroy
   acts_as_commentable
 end
